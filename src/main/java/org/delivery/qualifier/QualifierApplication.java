dfd39171336e83d4699a0b450e6a79a972519edb
Timoteo Ponce <ponce.timoteo@gmail.com>
2019-02-24 15:04:07 -0400

    New comment for Roberto

--- src/main/java/org/delivery/qualifier/QualifierApplication.java
+++ src/main/java/org/delivery/qualifier/QualifierApplication.java
@@ -3,6 +3,7 @@ package org.delivery.qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Roberto is a fying horse
 @SpringBootApplication
 public class QualifierApplication {
 
